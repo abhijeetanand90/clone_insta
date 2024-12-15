206b21a4a9e2b7920f61452f5893546a9c9e4476
Abhijeet <abhijeetanand407@gmail.com>
2024-12-15 10:32:41 +0530

    C:

---
+++ client/src/component/Auth/Auth.jsx
@@ -0,0 +1,16 @@
+import styles from './Auth.module.css'
+import Form from '../Form/Form'
+
+export default function Auth() {
+  return (
+    <div className={`${styles.body} ${styles.container} ${styles.flexAuth} ${styles.poppins}`}>
+      <div>
+            <img src="./auth.png" alt="" />
+        </div>
+        <div>
+            <Form />
+            <div>Get the app</div>
+        </div>
+    </div>
+  )
+}
---
+++ client/src/component/Form/Form.jsx
@@ -0,0 +1,26 @@
+import styles from './Form.module.css'
+
+function handleSubmit(e){
+    e.preventDefault();
+    console.log('submit')
+}
+
+export default function Form() {
+  return (
+    <div >
+        
+        
+    <form   onSubmit={handleSubmit}    className={styles.flexForm}>
+        <input placeholder="Phone number, username or email address" />
+        <input placeholder="Password" />
+        <button type='submit' className={styles.btnlogin}>Log in</button>
+        <p>__________ OR ___________</p>
+        <button className={styles.btngoogle}>Sign in with Google</button>
+        <button> Forgot Password?</button>
+    </form>
+
+
+
+    </div>
+  )
+}
